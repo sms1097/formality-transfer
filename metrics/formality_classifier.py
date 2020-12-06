b76ab9bbabbc8323337b20c95639bb66195a70c6
Sean Smith <smsmith714@gmail.com>
2020-12-06 14:07:38 -0500

    Formality classification metric and results

---
+++ metrics/formality_classifier.py
@@ -0,0 +1,43 @@
+import pickle
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+class FormalityClassifier:
+    def __init__(self):
+        with open('metrics/formality-misc/formality-tokenizer.pickle', 'rb') as handle:
+            self.tokenizer = pickle.load(handle)
+        
+        self.vocab_size = len(self.tokenizer.word_index) + 1
+        
+        self.model = tf.keras.Sequential([
+            tf.keras.layers.Embedding(self.vocab_size, 200, mask_zero=True),
+            tf.keras.layers.Dropout(0.8),  
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(1024, return_sequences=True)),
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(1024)),
+            tf.keras.layers.Dropout(0.8), 
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.Dense(2, activation='softmax')
+        ])
+        
+        self.model.load_weights('metrics/formality-misc/formality_classifier')
+        
+        
+    def _tokenize(self, corpus):
+        """ Tokenize data and pad sequences """
+
+        seqs = self.tokenizer.texts_to_sequences(corpus)
+        padded_seqs = pad_sequences(seqs, padding='post')
+
+        return padded_seqs,
+        
+
+    def classify(self, f_corpus):
+        tokenized_corpus = self._tokenize(f_corpus)
+        
+        results = self.model.predict(tokenized_corpus)
+        
+        return np.mean(results[:, 1])
