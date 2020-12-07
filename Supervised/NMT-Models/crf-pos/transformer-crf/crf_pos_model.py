10099178d8adae545c423ff30ce775c1f30fcec4
Sean Smith <sms1097@wildcats.unh.edu>
2020-12-06 19:08:09 -0500

    CRF POS model changes

--- Supervised/NMT-Models/crf-pos/transformer-crf/crf_pos_model.py
+++ Supervised/NMT-Models/crf-pos/transformer-crf/crf_pos_model.py
@@ -28,4 +28,31 @@ class CrfPosSeq2Seq(onmt.models.SequenceToSequence):
         )
 
 
-model = CrfPosSeq2Seq()
+class CrfPosTransformer(onmt.models.Transformer):
+    def __init__(self):
+        super().__init__(
+            source_inputter = onmt.inputters.ParallelInputter(
+                [onmt.inputters.WordEmbedder(embedding_size=512),
+                 onmt.inputters.WordEmbedder(embedding_size=512)],
+                reducer=onmt.layers.ConcatReducer()),
+            target_inputter = onmt.inputters.WordEmbedder(embedding_size=512),
+            num_layers=6,
+            num_units=512,
+            num_heads=8,
+            ffn_inner_dim=2048,
+            dropout=0.1,
+            attention_dropout=0.1,
+            ffn_dropout=0.1
+        )
+
+    def auto_config(self, num_replicas=1):
+        config = super().auto_config(num_replicas=num_replicas)
+        max_length = config["train"]["maximum_features_length"]
+        return onmt.utils.misc.merge_dict(config, {
+            "train": {
+                "maximum_features_length": [max_length, max_length]
+            }
+        })
+
+
+model = CrfPosTransformer()
