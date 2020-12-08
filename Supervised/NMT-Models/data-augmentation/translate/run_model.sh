9af04db0792d462f511258165fdbfaad92b1798e
Sean Smith <sms1097@wildcats.unh.edu>
2020-12-08 16:58:07 -0500

    More NMT Models

---
+++ Supervised/NMT-Models/data-augmentation/translate/run_model.sh
@@ -0,0 +1,1 @@
+onmt-main --model_type Transformer --config data.yaml --auto_config train --with_eval
---
+++ Supervised/NMT-Models/data-augmentation/translate/translate.sh
@@ -0,0 +1,3 @@
+onmt-main --config data.yaml --auto_config infer \
+	--features_file Data1/informal-test.txt \
+	--predictions_file back_translation.txt
