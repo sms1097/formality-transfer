f2b4489c8edd9bf343c3ae8753617ababc0b61d3
Sean Smith <smsmith714@gmail.com>
2020-12-12 16:22:29 -0500

    Massive file restructure

--- supervised/Multi-Encoder Transformer/crf-pos/transformer-crf/crf_pos_transformer.py
+++ supervised/Multi-Encoder Transformer/crf-pos/transformer-crf/crf_pos_transformer.py
@@ -1,13 +1,12 @@
 import opennmt as onmt
 
 
-class CrfPosTransformer(onmt.models.Transformer):
+class DualTransformer(onmt.models.Transformer):
     def __init__(self):
         super().__init__(
             source_inputter = onmt.inputters.ParallelInputter(
                 [onmt.inputters.WordEmbedder(embedding_size=512),
-                 onmt.inputters.WordEmbedder(embedding_size=512)],
-                reducer=onmt.layers.ConcatReducer()),
+                 onmt.inputters.WordEmbedder(embedding_size=512)]),
             target_inputter = onmt.inputters.WordEmbedder(embedding_size=512),
             num_layers=6,
             num_units=512,
@@ -18,5 +17,15 @@ class CrfPosTransformer(onmt.models.Transformer):
             ffn_dropout=0.1
         )
 
+    def auto_config(self, num_replicas=1):
+        config = super().auto_config(num_replicas=num_replicas)
+        max_length = config["train"]["maximum_features_length"]
+        return onmt.utils.misc.merge_dict(config, {
+            "train": {
+                "maximum_features_length": [max_length, max_length]
+            }
+        })
 
-model = CrfPosTransformer()
+
+model = DualTransformer()
+
